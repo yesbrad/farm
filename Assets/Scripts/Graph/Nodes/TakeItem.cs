356029d6876815826efc53dabb69bf81336a7180
brad <b.bradfrancis@gmail.com>
2020-04-17 16:18:16 +1000

    Added the Take Item node

---
+++ Assets/Scripts/Graph/Nodes/TakeItem.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XNode;
+
+[NodeTint("#00F0FF")]
+public class TakeItem : OceanNode
+{
+    public Item item;
+    public int amount;
+
+	public override void Use(Interactable interactable)
+	{
+        base.Use(interactable);
+
+        if (item != null)
+        {
+	        if (Inventory.instance.HasItem(item, amount))
+	        {
+		        for (int i = 0; i < amount; i++)
+	            {
+					Inventory.instance.RemoveItem(item);
+	            }
+	        }
+        }
+        else
+            Debug.Log("Missing Item From Node: " + id);
+        
+        NextNode();
+    }
+
+	public override void NextNode()
+	{
+        base.NextNode();
+	}
+}
